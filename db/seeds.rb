a2a12a75a335f1f06cbf622827e9d7823bf619a7
darrendaz <darren.lou@gmail.com>
2019-08-19 12:50:23 -0700

    did rake db:rollback, rake db:migrate, then rake db:seed again to start over with fresh seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+25.times do
+  Strain.create(name: Faker::Cannabis.strain)
+end
