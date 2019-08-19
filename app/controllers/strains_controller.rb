da50dd924dbe9d79722f7866a57d12b33516c573
darrendaz <darren.lou@gmail.com>
2019-08-19 12:28:08 -0700

    wrote index, show, and create actions in Strains controller

--- app/controllers/strains_controller.rb
+++ app/controllers/strains_controller.rb
@@ -1,2 +1,19 @@
 class StrainsController < ApplicationController
+  def index
+    @strains = Strain.all
+    render json: @strains
+  end
+
+  def show
+    @strain = Strain.find_by(id: params[:id])
+  end
+
+  def create
+    @strain = Strain.new(strain_params)
+  end
+
+  private
+  def strain_params
+    params.require(:strain).permit(:name)
+  end
 end
